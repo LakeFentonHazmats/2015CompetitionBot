7f643c3faacbc3a8f4ad26198426cddc94f1d48a
Jeremy2145 <jeremy.besser1@gmail.com>
2015-02-14 15:05:46 -0500

    LiftToHome and LiftToTop

---
+++ 2015Bot/src/org/usfirst/frc/team2145/robot/commands/LiftToHome.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team2145.robot.commands;
+
+import org.usfirst.frc.team2145.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LiftToHome extends Command{
+
+	@Override
+	protected void initialize() {
+		Robot.lift.liftEncoderReset();
+	}
+
+	@Override
+	protected void execute() {
+		if(Robot.lift.homeSwitchValue() != 1){
+			Robot.lift.liftDrive(-0.7);
+		}
+		else{
+			Robot.lift.liftDrive(0);
+		}
+	}
+
+	@Override
+	protected boolean isFinished() {
+		
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		
+		
+	}
+
+}
---
+++ 2015Bot/src/org/usfirst/frc/team2145/robot/commands/LiftToTop.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team2145.robot.commands;
+
+import org.usfirst.frc.team2145.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LiftToTop extends Command{
+	@Override
+	protected void initialize() {
+		Robot.lift.liftEncoderReset();
+	}
+
+	@Override
+	protected void execute() {
+		if(Robot.lift.liftEncoder() < 52){
+			Robot.lift.liftDrive(-0.7);
+		}
+		else{
+			Robot.lift.liftDrive(0);
+		}
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {	
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		
+	}
+
+}
